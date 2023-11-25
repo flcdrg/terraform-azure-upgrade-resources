80144333c1ecd2b76aa2c9ad014c4fe13ec68d56
David Gardiner <david@gardiner.net.au>
2023-11-25 16:23:14 +1030

    Add App Service

--- v2/app-service.tf
+++ v2/app-service.tf
@@ -8,3 +8,28 @@ resource "azurerm_app_service_plan" "plan" {
     size = "B1"
   }
 }
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/app_service
+resource "azurerm_app_service" "appservice" {
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+  name                = "appservice-tfupgrade-australiasoutheast"
+  location            = data.azurerm_resource_group.group.location
+  resource_group_name = data.azurerm_resource_group.group.name
+  https_only          = true
+
+  app_settings = {
+    "TEST" = "TEST"
+  }
+
+  site_config {
+    always_on                 = true
+    dotnet_framework_version  = "v6.0"
+    ftps_state                = "Disabled"
+    http2_enabled             = true
+    min_tls_version           = "1.2"
+    use_32_bit_worker_process = false
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
--- v2/versions.tf
+++ v2/versions.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   cloud {
     organization = "flcdrg"
     hostname     = "app.terraform.io"
